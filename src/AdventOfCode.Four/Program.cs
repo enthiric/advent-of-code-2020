4172c7ea024ed420a82081fb48e92d133c872213
Jurgen Heeffer <jurgen@newestindustry.nl>
2020-12-04 09:44:45 +0100

    Day four

---
+++ src/AdventOfCode.Four/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Four
+{
+    public class Code
+    {
+        public int Part1(string[] input)
+        {
+            var passports = new List<Passport>();
+            var data = string.Join("\n", input).Split("\n\n");
+            foreach (var passport in data)
+            {
+                passports.Add(Passport.Parse(passport));
+            }
+
+            return passports.Count(p => p.IsValid_Part1());
+        }
+        
+        public int Part2(string[] input)
+        {
+            var passports = new List<Passport>();
+            var data = string.Join("\n", input).Split("\n\n");
+            foreach (var passport in data)
+            {
+                passports.Add(Passport.Parse(passport));
+            }
+
+            return passports.Count(p => p.IsValid_Part2());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = new Code();
+            var input = File.ReadAllLines("input.txt");
+
+            Console.WriteLine(code.Part1(input));
+            Console.WriteLine(code.Part2(input));
+        }
+    }
+}
+
+// - required fields
+// byr (Birth Year)
+// iyr (Issue Year)
+// eyr (Expiration Year)
+// hgt (Height)
+// hcl (Hair Color)
+// ecl (Eye Color)
+// pid (Passport ID)
+// cid (Country ID)
