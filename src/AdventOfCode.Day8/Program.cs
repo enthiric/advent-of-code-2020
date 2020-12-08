9227ab278e8ce129ace3e3a83847db0a9d97c9d9
Jurgen Heeffer <jurgen@newestindustry.nl>
2020-12-08 09:59:58 +0100

    Day 8

---
+++ src/AdventOfCode.Day8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode.Day8
+{
+    public class Code
+    {
+        public int Part1(string[] input)
+        {
+            return BootCode.Parse(input).Part1();
+        }
+
+        public long Part2(string[] input)
+        {
+            return BootCode.Parse(input).Part2();
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = new Code();
+
+            Console.WriteLine(code.Part1(File.ReadAllLines("input.txt")));
+            Console.WriteLine(code.Part2(File.ReadAllLines("input-2.txt")));
+        }
+    }
+}
